9258f5d7bf522d06b0666b2bc301e219f3cd6d83
gebeto <slavik.nychkalo@gmail.com>
2022-01-19 23:22:50 +0200

    parse all tests

--- parser.py
+++ parser.py
@@ -1,5 +1,8 @@
 from xml.parsers.expat import ParserCreate
 from urllib.request import urlopen
+from urllib.parse import urlparse, urlsplit
+import json
+import os
 
 
 class MyParser:
@@ -20,5 +23,38 @@ def get_all_pages():
 	return parser.CharacterDataHandler.items
 
 
-pages = get_all_pages()
-print(pages[0])
+def load_all_pages():
+	pages = get_all_pages()
+	json.dump(pages, open("tests/index.json", "w"), indent=4)
+
+
+def get_page_title(url):
+	_url = urlparse(url)
+	root, file = os.path.split(_url.path)
+	name, ext = os.path.splitext(file)
+	return name
+
+
+def get_page_data(url):
+	res = urlopen(url).read()
+	try:
+		data = res.decode("utf-8")\
+			.split('<script id="sourceData" type="application/json">')[1]\
+			.split('</script>')[0]
+		return json.loads(data)
+	except:
+		pass
+	return None
+
+
+def parse_tests():
+	urls = json.load(open("tests/index.json"))
+	for url in urls:
+		filename = get_page_title(url)
+		data = get_page_data(url)
+		if data:
+			print(filename)
+			json.dump(data, open(f"tests/{filename}.json", "w"), indent=4)
+
+
+parse_tests()
